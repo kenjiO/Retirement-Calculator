b393c6340cc8a5abb48820b5abec9ec50f0c2f3d
Kenji Okamoto <kenjio@users.noreply.github.com>
2015-11-20 11:36:12 -0800

    Create SavingsYear class to model a year of retirement savings

---
+++ src/edu/westga/retirement/model/SavingsYear.java
@@ -0,0 +1,60 @@
+package edu.westga.retirement.model;
+
+/**
+ * Model the activity of one year of retirement savings
+ * @author Kenji Okamoto
+ * @version 20151120
+ *
+ */
+public final class SavingsYear {
+    private final int beginBalance;
+    private final int contribution;
+    private final int appreciation;
+    private final int endBalance;
+
+    /**
+     * Create a new year of savings
+     * @param beginBalance The savings balance at the start of the year
+     * @param contribution The contribution amount for the year
+     * @param appreciationRate The investment appreciation rate
+     */
+    public SavingsYear(int beginBalance, int contribution, double appreciationRate) {
+        this.beginBalance = beginBalance;
+        this.contribution = contribution;
+        this.appreciation = (int) (this.beginBalance * appreciationRate);
+        this.endBalance = this.beginBalance + this.contribution + this.appreciation;
+    }
+
+    /**
+     * Get the beginning balance for the year
+     * @return The balance at the start of the year
+     */
+    public int getBeginBalance() {
+        return this.beginBalance;
+    }
+
+    /**
+     * Get the contribution made for the year
+     * @return The contribution made for the year
+     */
+    public int getContribution() {
+        return this.contribution;
+    }
+
+    /**
+     * Get the appreciation on the savings for the year
+     * @return The amount the savings grew from appreciation
+     */
+    public int getAppreciation() {
+        return this.appreciation;
+    }
+
+    /**
+     * Get the end balance for the year
+     * @return The ending balance for the year
+     */
+    public int getEndBalance() {
+        return this.endBalance;
+    }
+
+}
---
+++ test/edu/westga/retirement/test/SavingsYearTest.java
@@ -0,0 +1,106 @@
+package edu.westga.retirement.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.westga.retirement.model.SavingsYear;
+
+/**
+ * Test class for the SavingsYear class
+ * @author Kenji Okamoto
+ * @version 20151120
+ *
+ */
+public class SavingsYearTest {
+
+    /**
+     * Test getBeginBalance is the same as the paramater passed in the constructor
+     */
+    @Test
+    public void testInitialYearStartBalanceEqualsParamater() {
+        SavingsYear testYear = new SavingsYear(95724, 1000, 7.5);
+        assertEquals(95724, testYear.getBeginBalance());
+    }
+
+    /**
+     * Test getContribution() is the same as the paramater passed in the constructor
+     */
+    @Test
+    public void testgetContributionIsSameAsConstructorParamter() {
+        SavingsYear testYear = new SavingsYear(100000, 8374, 0);
+        assertEquals(8374, testYear.getContribution());
+    }
+
+
+    /**
+     * Test that the contrubution is factored in to the end balance
+     */
+    @Test
+    public void testContributionIsFactoredIntoTheEndBalance() {
+        SavingsYear testYear = new SavingsYear(5000, 1000, 0);
+        assertEquals(6000, testYear.getEndBalance());
+
+    }
+
+    /**
+     * Test that a 0 contibution does not increase the balance
+     */
+    @Test
+    public void test0ContributionDoesNotIncreaseBalance() {
+        SavingsYear testYear = new SavingsYear(5000, 0, 0);
+        assertEquals(5000, testYear.getEndBalance());
+
+    }
+
+    /**
+     * Test that 0 contrubition and 0 appreciation rate does not change balance
+     */
+    @Test
+    public void test0ContributionAnd0AppreciationRateDoesNotChangeBalance() {
+        SavingsYear testYear = new SavingsYear(5000, 0, 0);
+        assertEquals(5000, testYear.getEndBalance());
+
+    }
+
+    /**
+     * Test getAppreciation returns begin balance * appreciationRate
+     */
+    @Test
+    public void testGetAppreciationIsBeginBalnceTimesAppreciationRate() {
+        SavingsYear testYear = new SavingsYear(5000, 1000, .06);
+        assertEquals(300, testYear.getAppreciation());
+    }
+
+    /**
+     * Test getAppreciation is 0 when the appreciationRate is 0
+     */
+    @Test
+    public void testGetAppreciationIs0WhenAppreciationRateIs0() {
+        SavingsYear testYear = new SavingsYear(5000, 1000, 0);
+        assertEquals(0, testYear.getAppreciation());
+    }
+
+    /**
+     * Test getAppreciation is 0 when begin balance is 0
+     */
+    @Test
+    public void testGetAppreciationWhenBeginBalanceIs0Is0() {
+        SavingsYear testYear = new SavingsYear(0, 1000, .06);
+        assertEquals(0, testYear.getAppreciation());
+    }
+
+    /**
+     * Test appreciation is factored into the end Balance
+     */
+    @Test
+    public void testAppreciationIsFactoredIntoTheEndBalance() {
+        SavingsYear testYear = new SavingsYear(5000, 0, .06);
+        assertEquals(5300, testYear.getEndBalance());
+    }
+
+
+
+
+
+}
